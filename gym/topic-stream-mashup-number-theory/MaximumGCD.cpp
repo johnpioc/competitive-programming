a21566cff13657d6b35046c2ad9657a629f8491d
John Pioc <johnpioc15@gmail.com>
2025-04-20 12:28:56 +1000

    gym: number theory practice problem a

---
+++ gym/topic-stream-mashup-number-theory/MaximumGCD.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int testCases;
+    cin >> testCases;
+
+    for (int t = 1; t <= testCases; t++) {
+        int n;
+        cin >> n;
+
+        cout << n / 2 << '\n';
+    }
+}
