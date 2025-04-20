5bf31f36bb9553968564945c2d3b29f292763f09
John Pioc <johnpioc15@gmail.com>
2025-04-20 12:48:23 +1000

    gym: number theory practice problem b

---
+++ gym/topic-stream-mashup-number-theory/EhabAndGcd.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int testCases;
+    cin >> testCases;
+
+    for (int t = 1; t <= testCases; t++) {
+        int x;
+        cin >> x;
+
+        cout << 1 << " " << x - 1 << "\n";
+    }
+}
