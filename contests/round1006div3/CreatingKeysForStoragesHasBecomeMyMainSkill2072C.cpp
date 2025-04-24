62365a6974170ca4d234cda9b5d00ca9ac69a868
John Pioc <johnpioc15@gmail.com>
2025-04-24 10:25:11 +1000

    contest round 998 div 3: problem a

---
+++ contests/round1006div3/CreatingKeysForStoragesHasBecomeMyMainSkill2072C.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int testCases;
+    cin >> testCases;
+
+    for (int t = 1; t<= testCases; t++) {
+        int n, x;
+        cin >> n >> x;
+    }
+}
