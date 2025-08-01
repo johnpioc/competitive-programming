72e05e14c4fb0b4ee49e44dbff1614feb0c57a70
John Pioc <johnpioc15@gmail.com>
2025-08-01 17:18:40 +1000

    neetcode 150: 100 same tree

---
+++ leetcode/neetcode-150/trees/SameTree100.cpp
@@ -0,0 +1,65 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == nullptr && q == nullptr) return true;
+        if (p != nullptr && q == nullptr) return false;
+        if (p == nullptr && q != nullptr) return false;
+        if (p->val != q->val) return false;
+
+        queue<TreeNode*> queueP;
+        queue<TreeNode*> queueQ;
+
+        queueP.push(p);
+        queueQ.push(q);
+
+        while (!queueP.empty() && !queueQ.empty()) {
+            int pNumToVisit = queueP.size();
+            int qNumToVisit = queueQ.size();
+
+            if (pNumToVisit != qNumToVisit) return false;
+
+            for (int i = 0; i < pNumToVisit; i++) {
+                TreeNode* pCurrent = queueP.front();
+                queueP.pop();
+
+                TreeNode* qCurrent = queueQ.front();
+                queueQ.pop();
+
+                // Check neighbours and see if they line up
+                if (pCurrent->left != nullptr && qCurrent->left == nullptr) return false;
+                if (pCurrent->left == nullptr && qCurrent->left != nullptr) return false;
+                if (pCurrent->right != nullptr && qCurrent->right == nullptr) return false;
+                if (pCurrent->right == nullptr && qCurrent->right != nullptr) return false;
+
+                if (pCurrent->left != nullptr && qCurrent->left != nullptr) {
+                    if (pCurrent->left->val != qCurrent->left->val) return false;
+                    else {
+                        queueP.push(pCurrent->left);
+                        queueQ.push(qCurrent->left);
+                    }
+                }
+
+                if (pCurrent->right != nullptr && qCurrent->right != nullptr) {
+                    if (pCurrent->right->val != qCurrent->right->val) return false;
+                    else {
+                        queueP.push(pCurrent->right);
+                        queueQ.push(qCurrent->right);
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+};
